0bcb37cf30f9e689c42831c766fab57ed40f6796
AzmiDawami24 <azmidawami@gmail.com>
2025-06-01 17:29:39 +0700

    update some content

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ checkbox.addEventListener('change', function () {
   body.classList.toggle('dark-mode');
   localStorage.setItem('theme', this.checked ? 'dark' : 'light');
 });
+
+
+const spotifyIframe = document.getElementById("spotify-embed");
+const setSpotifyTheme = () => {
+  const isDark = body.classList.contains('dark-mode');
+  const themeParam = isDark ? "0" : "1";
+  const playlistId = "0nZis5ePycLlX70IrDIX6o";
+  spotifyIframe.src = `https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator&theme=${themeParam}`;
+};
+
+// Panggil saat awal load
+setSpotifyTheme();
+
+// Update ketika toggle berubah
+checkbox.addEventListener('change', () => {
+  setSpotifyTheme();
+});
